722faa9d859600e40b89b903c51db86b03a2ce47
frappe <frappe@ubuntu.vm>
2020-08-30 10:24:44 +0000

    Add Employee checkin manual & project activity-wise salary regester report

---
+++ pav/pav/doctype/employee_checkin_manual/employee_checkin_manual.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Ahmed Mohammed Alkuhlani and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Employee Checkin Manual', {
+	// refresh: function(frm) {
+
+	// }
+});
