1d0650d75f4c823cb5d66ab5acb4a5247450ffb5
frappe <frappe@ubuntu.vm>
2021-03-15 19:45:51 +0000

    Delete utils codes

--- pav/pav/utils.py
+++ pav/pav/utils.py
@@ -8,18 +8,5 @@ from frappe.utils import getdate, add_days, today
 from frappe.model.document import Document
 
 
-def leave_auto_approve():
-	'''leave_auto_approve'''
-    enable_auto_approve_leave = frappe.db.get_single_value("PAV HR Settings", "enable_auto_approve_leave")
-    if enable_auto_approve_leave==1 :
-        from_workflow_state=frappe.db.get_single_value('PAV HR Settings', 'from_workflow_state')
-        to_workflow_state=frappe.db.get_single_value('PAV HR Settings', 'to_workflow_state')
-        period=frappe.db.get_single_value('PAV HR Settings', 'period')
-        leaves = frappe.get_all("Leave Application",
-	   		fields=["name"],
-	    	filters={'workflow_state' : from_workflow_state, 'posting_date':('<=',add_days(today(),(period-(period*2))))})
-		for leave in leaves:
-			frappe
-			leave_obj = frappe.get_doc("Leave Application",leave.name)
-			leave_obj.workflow_state = to_workflow_state
-			leave_obj.db_set("workflow_state", to_workflow_state, update_modified=False)
+def leave_auto_approve():	
+    return
